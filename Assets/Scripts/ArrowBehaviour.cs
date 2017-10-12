058fd1d0052697e461a5a66fade28a888253c5c3
Mykola Rybak <mykolarybachello@gmail.com>
2017-10-12 23:39:09 +0300

    start implement core

---
+++ Assets/Scripts/ArrowBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ArrowBehaviour : MonoBehaviour
+    {
+        private Game _game;
+
+        [SerializeField] private float _timeRotation = 10f;
+        [SerializeField] private float _deltaRotation = 15f;
+        [SerializeField] private float Speed = 1;
+
+        private const float hoursToDegrees = 360f / 12f;
+        private const float minutesToDegrees = 360f / 60f;
+        private const float secondsToDegrees = 360f / 60f;
+
+        private void Start ( ) {
+            Init();
+        }
+
+        private void Init ( ) {
+            //todo: implement refences
+            _game = Game.Instance;
+        }
+
+        private void Update ( ) {
+            if(Game.Pause) return;
+            UpdateArrowMovement();
+        }
+
+        private void UpdateArrowMovement ( ) {
+            var deltaAngle = Quaternion.Euler(0f, 0f, -_deltaRotation);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, transform.localRotation * deltaAngle,
+                _timeRotation * Time.deltaTime);
+        }
+
+        //todo: create time behaviour that increments by delta time multiply by some amount
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Game : MonoBehaviour
+    {
+        private static Game _instance;
+        private static bool _pause = false;
+
+        private static float _gameSpeed = 1f;
+
+        private void Awake ( ) {
+            _instance = this;
+        }
+
+        private void Update ( ) {
+            Time.timeScale = Speed;
+        }
+
+        private void OnDestroy ( ) {
+            _instance = null;
+        }
+
+        private static void OnGameStart ( ) {
+            //todo: implement here
+        }
+
+        private static void OnGameFinished ( ) {
+            //todo: implement here
+        }
+
+
+        public static float Speed
+        {
+            get { return _gameSpeed; }
+            set
+            {
+                _gameSpeed = value;
+
+                if (!Game.Pause) Time.timeScale = value;
+            }
+        }
+
+
+
+        public static bool Pause
+        {
+            get { return _pause; } 
+            set
+            {
+                _pause = value;
+            }
+        }
+
+
+        public static Game Instance {
+            get {
+                if (_instance)
+                    return _instance;
+                else {
+                    var dataGameOb = GameObject.Find("game manager");
+                    if (dataGameOb) {
+                        _instance = dataGameOb.GetComponent<Game>();
+                        return _instance;
+                    }
+                    var gameob = new GameObject();
+                    gameob.name = "game manager";
+                    _instance = gameob.AddComponent<Game>();
+                    return _instance;
+                }
+            }
+        }
+    }
+}
