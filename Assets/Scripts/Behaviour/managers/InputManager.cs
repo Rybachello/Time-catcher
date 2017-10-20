76b488ee050eda2409b49f2fe5d225de145b8cf4
Mykola Rybak <mykolarybachello@gmail.com>
2017-10-20 16:27:08 +0300

    refactor inputmanager

--- Assets/Scripts/Behaviour/managers/InputManager.cs
+++ Assets/Scripts/Behaviour/managers/InputManager.cs
@@ -4,21 +4,28 @@ namespace Assets.Scripts.Behaviour.managers
 {
     public class InputManager : MonoBehaviour
     {
-        private const float MinTimeSpeed = 0.75f;
-        private const float MaxTimeSpeed = 5f;
-
-        [SerializeField] private float _lerpTime = 1.25f;
+        private float _lerpTime = 0.5f;
         private float _currentLerpTime;
 
-        private Game _game;
+        private Camera _camera;
+        private Animator _animator;
 
         private float _currentSpeed;
         private bool _slowTime;
+        private float _currentSize;
 
-        private void Start ( ) {
-            _game = Game.Instance;
+        private const string anim_release = "relase";
+        private const string anim_press = "press";
+
+        private void Awake ( ) {
+            Init();
+        }
+
+        private void Init ( ) {
+            _animator = GetComponent<Animator>();
+            _camera = Camera.main;
+            _currentSize = _camera.orthographicSize;
             _currentSpeed = Game.Speed;
-            
         }
 
         private void Update ( ) {
@@ -36,29 +43,31 @@ namespace Assets.Scripts.Behaviour.managers
             var perc = _currentLerpTime / _lerpTime;
             float speed;
             if (_slowTime) {
-                speed = Mathf.Lerp(_currentSpeed, MinTimeSpeed, perc);
-                speed = speed < MinTimeSpeed ? MinTimeSpeed : speed;
-                Camera.main.orthographicSize = Mathf.Lerp(5.5f, 5f, perc);
-
+                speed = Mathf.Lerp(_currentSpeed, Constans.MinGameTimeSpeed, perc);
+                speed = speed < Constans.MinGameTimeSpeed ? Constans.MinGameTimeSpeed : speed;
+                //_camera.orthographicSize = Mathf.Lerp(_currentSize, Constans.MinCamaraOrthographicSize, perc);
             } else {
-                speed = Mathf.Lerp(MinTimeSpeed, _currentSpeed, perc);
-                speed = speed > MaxTimeSpeed ? MaxTimeSpeed : speed;
-                if(Camera.main.orthographicSize != 5.5f)
-                Camera.main.orthographicSize = Mathf.Lerp(5f, 5.5f, perc);
+                speed = Mathf.Lerp(Constans.MaxMaxTimeSpeed, _currentSpeed, perc);
+                speed = speed > Constans.MaxMaxTimeSpeed ? Constans.MaxMaxTimeSpeed : speed;
             }
+            _camera.orthographicSize = Mathf.Lerp(_currentSize,
+                _slowTime ? Constans.MinCamaraOrthographicSize : Constans.MaxCamaraOrthographicSize, perc);
             return speed;
         }
 
         void OnMouseDown ( ) {
             SlowTime = true;
+            _currentSize = _camera.orthographicSize;
+            _animator.Play("press");
             Debug.Log("[input] mouseDown");
         }
 
 
         void OnMouseUp ( ) {
             SlowTime = false;
-            _game.CatchTime();
-            EventManager.TriggerEvent(EventManagerType.CatchTime); //set an ivent
+            EventManager.TriggerEvent(EventManagerType.CatchTime); //set an ivent //todo: delete
+            _currentSize = _camera.orthographicSize;
+            _animator.Play("release");
             Debug.Log("[input] mouseUp");
         }
 
@@ -69,5 +78,14 @@ namespace Assets.Scripts.Behaviour.managers
                 _currentLerpTime = 0;
             }
         }
+    }
+
+    public class Constans
+    {
+        public static float MaxCamaraOrthographicSize = 6f;
+        public static float MinCamaraOrthographicSize = 5f;
+
+        public static float MinGameTimeSpeed = 0f;
+        public static float MaxMaxTimeSpeed = 2f;
     }
 }
