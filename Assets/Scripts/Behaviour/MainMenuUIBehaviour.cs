3bba515f096eaff30ab06e2e0cd6d33047980051
Mykola Rybak <mykolarybachello@gmail.com>
2017-11-16 13:28:23 +0200

    add main menu

---
+++ Assets/Scripts/Behaviour/MainMenuUIBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Behaviour
+{
+    public class MainMenuUIBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Button _exitButton;
+        [SerializeField] private Button _playButton;
+
+        private void Awake ( ) {
+            Init();
+        }
+
+        private void Init ( ) {
+            //buttons
+            _playButton = _playButton ?? gameObject.transform.Find("Btn_Play").GetComponent<Button>();
+            _exitButton = _exitButton ?? gameObject.transform.Find("Btn_Exit").GetComponent<Button>();
+        }
+
+        private void Start ( ) {
+            _playButton.onClick.AddListener(OnPlayButtonClick);
+            _exitButton.onClick.AddListener(OnExitButtonClick);
+        }
+
+        private void OnPlayButtonClick ( ) {
+            SceneManager.LoadScene("game");
+        }
+
+        private void OnExitButtonClick ( ) {
+            Application.Quit();
+        }
+    }
+}
